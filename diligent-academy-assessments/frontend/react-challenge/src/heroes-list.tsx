4e1355c85a559bef76164079af9df763ece90687
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-08-29 11:08:57 +0200

    add isLoading component to Heroes-list.tsx

--- diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
@@ -1,4 +1,5 @@
 import CurrentHero from "./components/CurrentHero";
+import LoadingMessage from "./components/LoadingMessage";
 
 interface Hero {
   id: number;
@@ -9,17 +10,22 @@ interface Hero {
 interface ReceivedProps {
   heroes: Hero[];
   handleAvailabilityChange: (heroId: number) => void;
+  isLoading: boolean;
+  error: string;
 }
 
 const HeroesList: React.FC<ReceivedProps> = ({
   heroes,
   handleAvailabilityChange,
+  isLoading,
+  error,
 }) => {
   console.log(heroes);
 
   return (
     <>
       <h2>Heroes</h2>
+      {isLoading && !error && <LoadingMessage />}
       <div style={{ display: "flex", flexWrap: "wrap", gap: "5px" }}>
         {heroes.map((hero) => (
           <div
